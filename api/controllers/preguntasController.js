23685610aae3092f90830370de0c862b397bbcfa
TheAlexBig <00062816@uca.edu.sv>
2018-07-01 16:57:14 -0600

    Fixing for the testing

--- api/controllers/preguntasController.js
+++ api/controllers/preguntasController.js
@@ -5,47 +5,47 @@ var mongoose = require('mongoose'),
   Task = mongoose.model('Preguntas');
 
 exports.list_all_pregunta = function(req, res) {
-  Pregunta.find({}, function(err, pregunta) {
+  Task.find({}, function(err, task) {
     if (err)
       res.send(err);
-    res.json(pregunta);
+    res.json(task);
   });
 };
 
 
 exports.create_a_pregunta = function(req, res) {
   var new_pregunta = new Pregunta(req.body);
-  new_pregunta.save(function(err, pregunta) {
+  new_pregunta.save(function(err, task) {
     if (err)
       res.send(err);
-    res.json(pregunta);
+    res.json(task);
   });
 };
 
 
 exports.read_a_pregunta = function(req, res) {
-  Pregunta.findById(req.params.preguntaId, function(err, pregunta) {
+  Task.findById(req.params.preguntaId, function(err, task) {
     if (err)
       res.send(err);
-    res.json(pregunta);
+    res.json(task);
   });
 };
 
 
 exports.update_a_pregunta = function(req, res) {
-  Pregunta.findOneAndUpdate({_id: req.params.preguntaId}, req.body, {new: true}, function(err, pregunta) {
+  Task.findOneAndUpdate({_id: req.params.preguntaId}, req.body, {new: true}, function(err, task) {
     if (err)
       res.send(err);
-    res.json(pregunta);
+    res.json(task);
   });
 };
 
 
 exports.delete_a_pregunta = function(req, res) {
 
-  Pregunta.remove({
+  Task.remove({
     _id: req.params.preguntaId
-  }, function(err, pregunta) {
+  }, function(err, task) {
     if (err)
       res.send(err);
     res.json({ message: 'Task successfully deleted' });
