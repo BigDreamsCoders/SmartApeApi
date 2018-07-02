9017296e17809d7bc259dcc3ee37d7c8d427d27a
TheAlexBig <00062816@uca.edu.sv>
2018-07-01 18:19:56 -0600

    Postman Works

--- api/models/PreguntaModel.js
+++ api/models/PreguntaModel.js
@@ -5,8 +5,10 @@ var Schema = mongoose.Schema;
 var PreguntasSchema = new Schema({
   Premisa: {type:String},
   TipoDePregunta:{type: Number},
-  TextoPregunta: { type: String},
-  Correcto: { type: Boolean},
+  Soluciones: [{
+    RespuestaPregunta: { type: String},
+    Correcto: { type: Boolean}
+  }],
   Created_date: {
     type: Date,
     default: Date.now
