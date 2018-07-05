4fd6dbf4c3189d5a8c80212860eca19af328297f
TheAlexBig <00062816@uca.edu.sv>
2018-07-04 20:41:03 -0600

    Removed old code line

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require('express'),
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://smartape:holaalex7.@cluster0-l1zkx.mongodb.net/test?');
-app.set('superSecret', config.secret);
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
